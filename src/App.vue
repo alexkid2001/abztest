e7098c35db3734c6f3e4c5a32c8bfc3d14559239
naydyonov <naydyonov@etm-system.ru>
2020-01-31 08:27:27 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <Header></Header>
+    <b-form-group
+            label-cols-sm="3"
+            label="Street:"
+            label-align-sm="right"
+            label-for="nested-street"
+    >
+      <b-form-input id="nested-street"></b-form-input>
+    </b-form-group>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,16 +16,20 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header";
+import "./styles/common.scss";
 
 export default {
   name: "app",
   components: {
+    Header,
     HelloWorld
   }
 };
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div
+    header.header
+        .header__wrap
+            .header__logo
+                img(src="/assets/images/logo.svg")
+        .top-menu
+    
+
+</template>
+
+<script>
+export default {
+  name: "TopMenu"
+};
+</script>
+
+<style scoped></style>
